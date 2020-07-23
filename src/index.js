35797a90bfc0c048b793f663538e8fa7a180a84c
Eduardo Bisinella <bisinella7@gmail.com>
2020-07-22 22:00:12 -0300

    add a uuidv4

--- src/index.js
+++ src/index.js
@@ -3,23 +3,36 @@ const { request } = require("express");
 
 const app = express();
 
-app.get("/projects", (request, response) => {
-    return response.json(["Projeto 1", "Projeto 2"]);
-});
+const projects = [];
 
+app.get("/projects", (request, response) => {
+//   const { title, owner } = request.query;
+
+//   console.log(title);
+//   console.log(owner);
+//   console.log(request.query);
+
+  return response.json(projects);
+});
 app.post("/projects", (request, response) => {
-    return response.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
+    const {title, owner} = request.body;
+
+  const projects = {title, owner};
+  return response.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
 });
 
 app.put("/projects/:id", (request, response) => {
-    return response.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
+    const { id } = request.params;
+    
+
+  return response.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
 });
 
 app.delete("/projects/:id", (request, response) => {
-    return response.json(["Projeto 2", "Projeto 3"]);
+  return response.json(["Projeto 2", "Projeto 3"]);
 });
 
-const port = 3333
+const port = 3333;
 app.listen(3333, () => {
-    console.log(`🚀 Server up and running  on PORT ${port} 🚀 `)
+  console.log(`🚀 Server up and running  on PORT ${port} 🚀 `);
 });
