fe8617c199a1c08383c5f452292480bb4cf4906a
Eduardo Bisinella <bisinella7@gmail.com>
2020-07-22 22:59:32 -0300

    new params

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { request } = require("express");
+// const { request } = require("express");
+const {uuid} = require("uuidv4")
 
 const app = express();
+app.use(express.json())
 
 const projects = [];
 
@@ -17,15 +19,29 @@ app.get("/projects", (request, response) => {
 app.post("/projects", (request, response) => {
     const {title, owner} = request.body;
 
-  const projects = {title, owner};
-  return response.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
+  const project = {id:uuid(), title, owner };
+  projects.push(project)
+  return response.json(project);
 });
 
 app.put("/projects/:id", (request, response) => {
     const { id } = request.params;
-    
+    const {title, owner} = request.body;
 
-  return response.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
+    const projectIndex = projects.findIndex((project) => project.id === id);
+    if (projectIndex < 0) {
+        return response.status(400).json({ error: "Project not found."})
+    }
+
+    const project = {
+        id,
+        title,
+        owner
+    };
+    projects[projectIndex] = project;
+    return response.json(project)
+
+//   return response.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
 });
 
 app.delete("/projects/:id", (request, response) => {
