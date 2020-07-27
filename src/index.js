6e83fe37be5017a6b4fdd72bbc28c85f713d2c13
Eduardo Bisinella <bisinella7@gmail.com>
2020-07-27 19:50:38 -0300

    add route delete

--- src/index.js
+++ src/index.js
@@ -8,56 +8,52 @@ app.use(express.json());
 const projects = [];
 
 app.get("/projects", (request, response) => {
-    const { title, owner } = request.query;
+  const { title, owner } = request.query;
 
-    console.log(title);
-    console.log(owner);
-    console.log(request.query);
+  console.log(title);
+  console.log(owner);
+  console.log(request.query);
 
-    return response.json(projects);
+  return response.json(projects);
 });
 app.post("/projects", (request, response) => {
-    const { title, owner } = request.body;
+  const { title, owner } = request.body;
 
-    const project = { id: uuid(), title, owner };
-    projects.push(project);
-    return response.json(project);
+  const project = { id: uuid(), title, owner };
+  projects.push(project);
+  return response.json(project);
 });
 
 app.put("/projects/:id", (request, response) => {
-    const { id } = request.params;
-    const { title, owner } = request.body;
+  const { id } = request.params;
+  const { title, owner } = request.body;
 
-    const projectIndex = projects.findIndex((project) => project.id === id);
-    if (projectIndex < 0) {
-        return response.status(400).json({ error: "Project not found." });
-    }
+  const projectIndex = projects.findIndex((project) => project.id === id);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found." });
+  }
 
-    const project = {
-        id,
-        title,
-        owner,
-    };
-    projects[projectIndex] = project;
-    return response.json(project);
-
-    //   return response.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
+  const project = {
+    id,
+    title,
+    owner,
+  };
+  projects[projectIndex] = project;
+  return response.json(project);
 });
 
 app.delete("/projects/:id", (request, response) => {
-    const { id } = request.params;
-    const { title, owner } = request.body;
+  const { id } = request.params;
 
-    const project = {
-        id,
-        title,
-        owner,
-    };
-    return response.send();
-    // return response.json(["Projeto 2", "Projeto 3"]);
+  const projectIndex = projects.findIndex((project) => project.id === id);
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: "Project not found." });
+  }
+  projects.splice(projectIndex, 1);
+  return response.status(204).send();
 });
 
 const port = 3333;
 app.listen(3333, () => {
-    console.log(`🚀 Server up and running  on PORT ${port} 🚀 `);
+  console.log(`🚀 Server up and running  on PORT ${port} 🚀 `);
 });
